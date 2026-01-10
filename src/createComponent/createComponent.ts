d61f52411f1dc4f890d5f9ca7d886e03d7c74f01
NextKolya <phelixgluy@gmail.com>
2026-01-10 13:44:18 +0600

    add createComponent util

---
+++ src/createComponent/createComponent.ts
@@ -0,0 +1,40 @@
+import { mkdir } from 'node:fs/promises';
+import { write } from 'bun';
+
+/**
+ *
+ * Creates react component directory, component file, CSS module file there.
+ *
+ * @param path - path of new component directory. For example, `./`.
+ *
+ * @param name - name of component. For example, `Button`.
+ */
+export const createComponent = (
+    path: string,
+    name: string
+): Promise<(number | void)[]> => {
+    const componentDirPath = path + name + '/';
+
+    return mkdir(componentDirPath).then(() => {
+        return Promise.all([
+            write(
+                componentDirPath + name + '.tsx',
+                'import __COMPONENT_STYLES__ from "./' +
+                    name +
+                    `.module.scss";'
+export default function` +
+                    name +
+                    `() {
+    return <div> </div>;            
+            `
+            ),
+
+            write(componentDirPath + name + '.module.scss', ''),
+
+            write(
+                componentDirPath + 'index.ts',
+                'export { default } from "./' + name + '";'
+            ),
+        ]);
+    });
+};
